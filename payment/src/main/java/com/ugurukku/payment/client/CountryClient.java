cb3784cc53fe33889df2ad6e130999e4f3823298
Ugur <ugurkarimli1@gmail.com>
2023-01-29 04:36:54 +0400

    20% finish

--- payment/src/main/java/com/ugurukku/payment/client/CountryClient.java
+++ payment/src/main/java/com/ugurukku/payment/client/CountryClient.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@Component
 @FeignClient(name = "country",url = "${client.country.endpoint}")
 public interface CountryClient {
 
